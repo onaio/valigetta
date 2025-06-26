06f2b5ede36f0d9c0e09e8381cf1140ae6821789
Kelvin Muchiri <kelvinmuchiri63@gmail.com>
2025-06-26 11:34:11 +0300

    make __version__ single source of truth

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
+import re
+from pathlib import Path
+
 from setuptools import setup
 
-setup()
+
+def get_version():
+    content = Path("valigetta/__init__.py").read_text()
+    match = re.search(r'__version__ = ["\'](.+?)["\']', content)
+    if not match:
+        raise RuntimeError("Cannot find __version__ in valigetta/__init__.py")
+    return match.group(1)
+
+
+setup(
+    setup_cfg=True,
+    version=get_version(),
+)
